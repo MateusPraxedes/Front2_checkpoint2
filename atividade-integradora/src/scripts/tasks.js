1db642f8bf93d5a96ee9096cd729f7cd550505a4
mateuspraxedes <mateuspr97@gmail.com>
2022-04-09 20:52:20 -0300

    atividade integradora em andamento

--- atividade-integradora/src/scripts/tasks.js
+++ atividade-integradora/src/scripts/tasks.js
@@ -3,16 +3,9 @@ let senha = document.querySelector("#senha");
 let formLogin = document.querySelector(".form_login");
 let body = document.querySelector("body");
 let btn = document.querySelector("#btn");
-let nome = document.querySelector("#nome");
-let sobrenome = document.querySelector("#sobrenome");
-let emailCadastro = document.querySelector("#email_cadastro");
-let senhaCadastro = document.querySelector("#senha_cadastro");
-let btnCriarConta = document.querySelector(".criarConta");
-let formCadastro = document.querySelector(".form_cadastro");
 
 window.addEventListener("load", (e) => console.log(e));
 const API_URL = "https://ctd-todo-api.herokuapp.com/v1";
-
 
 function criarli() {
   let li = document.createElement("li");
@@ -21,9 +14,9 @@ function criarli() {
   return li;
 }
 
-function configuracoes(body) {
+function configuracoes(method, body) {
   let configuracoes = {
-    method: "POST",
+    method: method,
     body: JSON.stringify(body),
     headers: {
       "Content-type": "application/json",
@@ -53,7 +46,7 @@ function logarUsuario(e) {
     password: senha.value,
   };
 
-  fetch(`${API_URL}/users/login`, configuracoes(body))
+  fetch(`${API_URL}/users/login`, configuracoes("POST", body))
     .then((r) => r.json())
     .then((r) => console.log(r));
 }
@@ -63,6 +56,13 @@ if (formLogin) {
 }
 
 // Criar uma novo usuário e Validaçao dos campos de cadastro
+
+let nome = document.querySelector("#nome");
+let sobrenome = document.querySelector("#sobrenome");
+let emailCadastro = document.querySelector("#email_cadastro");
+let senhaCadastro = document.querySelector("#senha_cadastro");
+let btnCriarConta = document.querySelector(".criarConta");
+let formCadastro = document.querySelector(".form_cadastro");
 
 function criarUsuario(e) {
   e.preventDefault();
@@ -91,7 +91,7 @@ function criarUsuario(e) {
     password: senhaCadastro.value,
   };
 
-  fetch(`${API_URL}/users`, configuracoes(body))
+  fetch(`${API_URL}/users`, configuracoes("POST", body))
     .then((r) => r.json())
     .then((r) => console.log(r));
 }
@@ -99,3 +99,62 @@ function criarUsuario(e) {
 if (formCadastro) {
   formCadastro.addEventListener("submit", criarUsuario);
 }
+
+// Criar uma nova tarefa
+
+let novaTarefa = document.querySelector(".nova-tarefas");
+let formNovaTarefas = document.querySelector(".form-tarefas");
+
+function criarTarefa(e) {
+  e.preventDefault();
+  let body = {
+    description: novaTarefa.value,
+    completed: false,
+  };
+
+  fetch(`${API_URL}/tasks`, configuracoes("POST", body))
+    .then((r) => r.json())
+    .then((r) => console.log(r));
+}
+
+if (formNovaTarefas) {
+  formNovaTarefas.addEventListener("submit", criarTarefa);
+}
+
+// Deleter tarefa
+
+function deletarTarefa(id) {
+  fetch(`${API_URL}/tasks{${id}}`)
+    .then((r) => r.json())
+    .then((r) => console.log(r));
+}
+
+// Atualizar uma tarefa existente
+
+function AtualizarTarefa(id) {
+  let body = {
+    description: novaTarefa.value,
+    completed: false,
+  };
+
+  fetch(`${API_URL}/tasks{${id}}`, configuracoes("PUT", body))
+    .then((r) => r.json())
+    .then((r) => console.log(r));
+}
+
+// Obter um determinada tarefa
+
+function obterTarefa(id){
+  fetch(`${API_URL}/tasks{${id}}`)
+  .then((r) => r.json())
+  .then((r) => console.log(r));
+}
+
+// Listar tatefas
+
+function listarTarefas(){
+  fetch(`${API_URL}/tasks`)
+  .then((r) => r.json())
+  .then((r) => console.log(r));
+}
+
